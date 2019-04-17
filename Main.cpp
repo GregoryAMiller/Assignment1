15e452ae51f851805866fc42b1f2f9a757053a3f
GregoryAMiller <32182450+GregoryAMiller@users.noreply.github.com>
2019-04-16 22:38:02 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,5 @@
+// Gregory Miller
+// Section 2
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,94 +13,148 @@ struct person {
     float balance;
 };
 
+
 int numberOfRecords(int);
-void fillPerson(person P[], int);
-void display(person P[], int);
-void findRichest(person P[], int);
-void deposit(char custName[], person P[], int);
-void newCopy(string file, person P[], int);
+person *readData(int);
+void printmenu();
+void display(person *arr, int);
+void findRichest(person *arr, int);
+void getDepositInfo(person *arr, int, char *custName, double *amount, int *cmpValue);
+void deposit(person *arr, int, char *custName, double);
+void newCopy(string, person *arr, int);
 
 int main() {
-    int n = 0;
-    int numAccounts = numberOfRecords(n);
-    person P[numAccounts];
-    fillPerson(P, numAccounts);
-    display(P, numAccounts);
-    findRichest(P, numAccounts);
-    char custName[20];  // we need to make a char array so we can pass to our function
-    cout << "Enter your full name to deposit money: ";
-    cin.getline(custName, 20);  // we want to make sure that the name they enter can fit into our char name[20]
-    deposit(custName, P, numAccounts);
-    newCopy("data.txt", P, numAccounts);
-    
-    return 0;
-}
 
-int numberOfRecords(int n){ // find the ammount of people are in the file
+      int choice;
+      int i = 0;
+      int N = numberOfRecords(i);
+      person *arr = readData(N);
+      char custName[19];
+      double amount = 0;
+      int cmpValue = -1;
+      do
+      {
+          printmenu();
+          cin >> choice;
+          switch(choice)
+          {
+              case 1:
+                  display(arr, N);
+                  break;
+
+              case 2:
+                  cin.ignore();
+                  getDepositInfo(arr, N, custName, &amount, &cmpValue);
+                  if (cmpValue == -1) { break;}
+                  deposit(arr, N, custName, amount);
+                  break;
+
+              case 3:
+                  findRichest(arr, N);
+                  break;
+
+              case 4:
+                  newCopy("data.txt", arr, N);
+                  cout << "File Updated..." << endl;
+                  break;
+
+              case 5:
+                  newCopy("data.txt", arr, N);
+                  break;
+
+              default:
+                  cout << "Invalid entry" << endl;
+                  break;
+          }
+          cout << endl;
+     } while(choice != 5);
+        arr = NULL;
+        delete []arr;
+        return 0;
+}
+  void printmenu() {
+      cout << "Please enter a choice:" << endl;
+      cout << "1. Display records"<< endl;
+      cout << "2. Deposit funds"<< endl;
+      cout << "3. Find Highest Balance" << endl;
+      cout << "4. Update records" << endl;
+      cout << "5. Exit the program" << endl;
+  }
+
+int numberOfRecords(int i){
     ifstream myData;
     myData.open("data.txt");
     string line;
-    while(myData){  // while we can get data from the file
-        getline(myData, line);  // take a line
-        n++;    // increase counter
-    }
-    n--;
-    myData.close();
-    return n;
-    
-}
-
-void fillPerson(person P[], int numAccounts){       // take in data from the file and fill the struct array
-    string firstName = "";
-    string lastName = "";
-    int i = 0;  // initialize counter
-    ifstream myData;
-    myData.open("data.txt");    // open file
     while(myData){
-        myData >> firstName >> lastName >> P[i].balance;    // take in data from array since firstName and lastName is a string and not a c-string
-        // we can direclty put in the last variable into struct array in savings
-        strcpy(P[i].name, (firstName + " " + lastName).c_str());    // combine firstName and lastName into a c-string and copy that into our char name array
-        i++;    // we need to use counter to iterate and fill char name array
+        getline(myData, line);
+        i++;
     }
-    myData.close(); // close file
+    i--;
+    myData.close();
+    return i;
+
 }
 
-void display(person P[], int numAccounts){  // display the struct array
+person *readData(int N){
+  string firstName;
+  string lastName;
+  person *P = new person[N];
+  ifstream dataIn;
+  dataIn.open("data.txt");
+  int i = 0;
+  while(dataIn >> firstName >> lastName >> P[i].balance){
+    // dataIn >> firstName >> lastName >> P[i].balance;
+    strcpy(P[i].name, (firstName + " " + lastName).c_str());
+    i++;
+  }
+  dataIn.close();
+
+  return P;
+}
+
+void getDepositInfo(person *arr, int N, char *custName, double *amount, int *cmpValue) {
+    cout << "Enter Name: ";
+    cin.getline(custName, 19);
+    for(int i = 0; i < N; i++){
+        if(strcmp(custName, arr[i].name) == 0) { *cmpValue = 0; break;}
+    }
+    if(*cmpValue == -1) { cout << "The name you entered does not match our records, please try again! "; return;}
+    cout << "Amount: ";
+    double depositAmount = 0;
+    cin >> depositAmount;
+    *amount = depositAmount;
+
+}
+void display(person *arr, int N){
     cout << "Name                Balance" << endl;
     cout << "---------------------------" << endl;
-    for(int i = 0; i < numAccounts; i++){   // loop through the struct array and cout each ones name and savings
-        cout << setw(20) << left << P[i].name << left << fixed << setprecision(2) << P[i].balance << endl;
+    for(int i = 0; i < N; i++){
+        cout << setw(20) << left << arr[i].name << left << fixed << setprecision(2) << arr[i].balance << endl;
     }
 }
 
-void findRichest(person P[], int numAccounts){  // finds the richest person and displays who they are
-    int temp = 0;   // temp variable so we can take index of greater valued
-    for(int i = 0; i < numAccounts; i++){   // loop through everyones savings and if we find a greater savings make temp that index's number
-        if(P[temp].balance < P[i].balance) { temp = i;}
-    }
-    cout << "The customer with maximum balance is " << P[temp].name << endl;    // cout the richest person
-}
-
-void deposit(char custName[], person P[], int numAccounts){ // pick the person you want to deposit and increase their savings by that ammount
-    int cmpValue = -1;   // since strcmp returns -1 or 1 if the strings are not equal and returns 0 if they are we want the base case to be false(-1/1)
+void findRichest(person *arr, int N){
     int temp = 0;
-    // i used to have cmpValue = 1 but if index 1 of P then we would get a bug and would cout the name entered does not match always
-    for(int i = 0; i < numAccounts; i++){   // loop through the struct array
-        if(strcmp(custName, P[i].name) == 0) { cmpValue = 0; temp = i; break;}    // if when we compare the name given to the names in the struct array and there is a match (strcmp returns 0) so if it returns 0 then we want to change our cmpValue so below code doesnt go off and set a temp variable to the index we find the name in
+    for(int i = 0; i < N; i++){
+        if(arr[temp].balance < arr[i].balance) { temp = i;}
     }
-    if(cmpValue == -1) { cout << "The name you entered does not match our records"; return;} // if we didnt find a match
-    cout << P[temp].name << ", how much would you like to deposit? ";
-    float depositAmount = 0;
-    cin >> depositAmount;
-    P[temp].balance += depositAmount;   // increase the persons savings by ammount they input
-    cout << "Your new balance is " << P[temp].balance << endl;
+    cout << "Highest balance: " << arr[temp].name << endl;
 }
 
-void newCopy(string file, person P[], int numAccounts){ // make a new copy of the file with updated savings
+void deposit(person *arr, int N, char *custName, double amount){
+    int temp = 0;
+    for(int i = 0; i < N; i++){
+        if(strcmp(custName, arr[i].name) == 0) {temp = i; break;}
+    }
+    arr[temp].balance += amount;
+    cout << "New balance: " << arr[temp].balance << endl;
+}
+
+void newCopy(string file, person *arr, int N){
     ofstream data;
     data.open(file);
-    for(int i = 0; i < numAccounts; i++){   // loop through struct and input new data
-        data << P[i].name << " " << fixed << setprecision(2) << P[i].balance << endl;
+    for(int i = 0; i < N; i++){
+        data << arr[i].name << " " << fixed << setprecision(2) << arr[i].balance << endl;
     }
     data.close();
 }
